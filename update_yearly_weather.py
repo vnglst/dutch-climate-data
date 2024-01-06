908ff8af9d3e35c21a32efc8af2c4a409a712faf
Koen van Gilst <koen@koenvangilst.nl>
2024-01-06 19:55:14 +0100

    add last temp trend

--- update_yearly_weather.py
+++ update_yearly_weather.py
@@ -101,6 +101,7 @@ def process_anomalies(df):
         'temperature_worst_case': 14.9 - df['tg_mean'].iloc[-1],
         'temperature_best_case': 11.4 - df['tg_mean'].iloc[-1],
         'last_temperature_anomaly': df['tg_anomaly'].iloc[-1],
+        'last_temperature_trend': df['tg_anomaly_trend'].iloc[-1],
         'years': years,
         'forecast_years': years + [str(year) for year in range(last_year+1, 2101)]
     }
